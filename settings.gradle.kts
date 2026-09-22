7aa37b92c9d6ebe8e1935365309a15378f4c126e
hgffdkhn-dot <hgffdkhn-dot@users.noreply.github.com>
2026-09-22 23:58:42 +0800

    first commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,9 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    // 必须是 PREFER_SETTINGS：Kotlin/Native 插件在配置期会往工程里加一个 ivy 仓库
+    // 用来下载 konan 工具链，FAIL_ON_PROJECT_REPOS 会直接把它判为非法
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
